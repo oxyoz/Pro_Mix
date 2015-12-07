3c7dd20ba7752c92624d107df0b021f20a33ac2e
oz <857527916@qq.com>
2015-12-07 18:30:57 +0800

    jni

---
+++ Jni_1/obj/local/x86/objs/a/a.o.d
@@ -0,0 +1,13 @@
+obj/local/x86/objs/a/a.o: jni/a.c \
+ F:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/jni.h \
+ F:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ F:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ F:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/android/api-level.h
+
+F:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/jni.h:
+
+F:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+F:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+F:/android-ndk-r10e/platforms/android-9/arch-x86/usr/include/android/api-level.h:
---
+++ Jni_1/obj/local/x86_64/objs/a/a.o.d
@@ -0,0 +1,13 @@
+obj/local/x86_64/objs/a/a.o: jni/a.c \
+ F:/android-ndk-r10e/platforms/android-21/arch-x86_64/usr/include/jni.h \
+ F:/android-ndk-r10e/platforms/android-21/arch-x86_64/usr/include/sys/cdefs.h \
+ F:/android-ndk-r10e/platforms/android-21/arch-x86_64/usr/include/sys/cdefs_elf.h \
+ F:/android-ndk-r10e/platforms/android-21/arch-x86_64/usr/include/android/api-level.h
+
+F:/android-ndk-r10e/platforms/android-21/arch-x86_64/usr/include/jni.h:
+
+F:/android-ndk-r10e/platforms/android-21/arch-x86_64/usr/include/sys/cdefs.h:
+
+F:/android-ndk-r10e/platforms/android-21/arch-x86_64/usr/include/sys/cdefs_elf.h:
+
+F:/android-ndk-r10e/platforms/android-21/arch-x86_64/usr/include/android/api-level.h:
